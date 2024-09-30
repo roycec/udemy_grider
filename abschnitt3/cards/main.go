395e473800fc21cf0853bf4949eebaf4b43084e4
Christian Reuss <christian.reuss@wwk.de>
2024-09-30 18:35:36 +0200

    Create slice and iterate over it

--- abschnitt3/cards/main.go
+++ abschnitt3/cards/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	//fmt.Println(cards)
+	for i, cards := range cards {
+		fmt.Println(i, cards)
+	}
 }
 
 func newCard() string {
