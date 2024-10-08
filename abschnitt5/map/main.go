847dacb8a857cf4b47358c4e5d5b99bb9e0545a9
Christian Reuss <creuss@shaizen.de>
2024-10-08 21:27:33 +0200

    Create function for iterating over a map

--- abschnitt5/map/main.go
+++ abschnitt5/map/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
 
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// colors := map[string]string{
-	//	"red":   "#ff0000",
-	//  "green": "#4bf745",
-	// }
+	printMap(colors)
+}
 
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hexcode := range c {
+		fmt.Println("Hex code for", color, "is", hexcode)
+	}
 }
